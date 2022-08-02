bcfaabbabee4dc59eb40cae3d6979c176c9d2eeb
baoyongan <baoyongan@zhongchebaolian.com>
2022-08-02 14:32:14 +0800

    表达式

--- java-eg-base/src/main/java/com/baoyongan/java/eg/base/numberAndString_ch/NumberTest.java
+++ java-eg-base/src/main/java/com/baoyongan/java/eg/base/numberAndString_ch/NumberTest.java
@@ -1,5 +1,6 @@
 package com.baoyongan.java.eg.base.numberAndString_ch;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 /**
@@ -9,33 +10,33 @@ public class NumberTest {
 
     public static void main(String[] args) {
 
-        Short i=new Short((short) -1);
+        Short i = new Short((short) -1);
         System.out.println(Short.toUnsignedInt(i));
 
         // int 值溢出，不会报错，因为符号位变成1，所以变成了负值
-        int max=Integer.MAX_VALUE;
-        int sm=max+1;
-        System.out.println("测试溢出情况"+sm);
+        int max = Integer.MAX_VALUE;
+        int sm = max + 1;
+        System.out.println("测试溢出情况" + sm);
         // 典型的测试
-        int lowest=-2147483648;
-        int low=Integer.MIN_VALUE;
+        int lowest = -2147483648;
+        int low = Integer.MIN_VALUE;
         int abs = Math.abs(lowest);
-        System.out.printf("int 的最小值：%d, %d,使用math.abs 取绝对值返回：%d,溢出情况 \n",lowest,low,abs);
+        System.out.printf("int 的最小值：%d, %d,使用math.abs 取绝对值返回：%d,溢出情况 \n", lowest, low, abs);
         // 负数的取余数运算注意项
         // (a/b)*b+a%b=a
-        System.out.printf("-14/3= %d \n",-14/3);
-        System.out.printf("14/-3= %d \n",14/-3);
-        System.out.printf("-14%%3= %d \n",-14%3);
-        System.out.printf("14%%-3= %d \n",14%-3);
+        System.out.printf("-14/3= %d \n", -14 / 3);
+        System.out.printf("14/-3= %d \n", 14 / -3);
+        System.out.printf("-14%%3= %d \n", -14 % 3);
+        System.out.printf("14%%-3= %d \n", 14 % -3);
 
-        BigInteger bigInteger=new BigInteger("1212132132132212");
+        BigInteger bigInteger = new BigInteger("1212132132132212");
 
 //        String s = Integer.toHexString(1);
 //        System.out.println(s);
 
 //        System.out.println(Integer.parseInt());
 //        int a=0x0f;
-        int a=0x7FFFFFFF;
+        int a = 0x7FFFFFFF;
         System.out.println(a);
         System.out.println(Integer.parseInt("F", 16));
         System.out.println(Integer.toHexString(15));
@@ -43,6 +44,24 @@ public class NumberTest {
         System.out.println(s.length());
 //        System.out.println(Integer.toBinaryString(a));
 
+        System.out.println("算法4_1.1.1 习题");
+        int A = (0 + 15) / 2;
+        double B = 2.0e-6 * 100000000.1; // 2.0e-6 表示2.0*10^-6;
+        double B_1 = 2.0 * (Math.pow(10, -6)) * 100000000.1;
+        boolean C = true && false || true && true;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(B_1);
+        System.out.println(C);
+        System.out.println(new BigDecimal("2.0e-6").toPlainString());
 
+        double v = (1 + 2.236) / 2;
+        System.out.println(v);
+        double v1 = 1 + 2 + 3 + 4.0;
+        System.out.println(v1);
+        boolean b = 4.1 >= 4;
+        System.out.println(b);
+        String s1 = 1 + 2 + "3";
+        System.out.println(s1);
     }
 }
