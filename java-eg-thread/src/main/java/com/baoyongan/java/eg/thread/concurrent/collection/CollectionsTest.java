08c3df6dfed64222d55b52ffde647e1a6b40fc19
baoyongan <baoyongan@zhongchebaolian.com>
2022-08-15 17:42:41 +0800

    consistentMap.putifabert

--- java-eg-thread/src/main/java/com/baoyongan/java/eg/thread/concurrent/collection/CollectionsTest.java
+++ java-eg-thread/src/main/java/com/baoyongan/java/eg/thread/concurrent/collection/CollectionsTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 并发集合
@@ -22,10 +23,51 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class CollectionsTest
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // 整个实例加锁（synchronized）
         List<String> list= Collections.synchronizedList(new ArrayList<String>());
         // 大量的CAS操作和分段加锁。
-        ConcurrentHashMap a=new ConcurrentHashMap();
+
+
+        ConcurrentHashMap<String,Integer> a=new ConcurrentHashMap();
+
+        int s=a.computeIfAbsent("ba",k->{
+            return (int)k.charAt(0);
+        });
+        System.out.println(s);
+        int ss=a.computeIfAbsent("cc",k->{
+            return (int)k.charAt(0);
+        });
+        System.out.println(ss);
+
+        int sss=a.computeIfAbsent("ba",k->{
+            return (int)k.charAt(0);
+        });
+        System.out.println(sss);
+        a.forEach((k,v)->{
+            System.out.printf("key : %s, Value: %d\n",k,v);
+        });
+
+        ConcurrentHashMap<String, AtomicInteger> as=new ConcurrentHashMap();
+        new Thread(()->{
+            AtomicInteger v=as.computeIfAbsent("a",k-> new AtomicInteger(0));
+            v.incrementAndGet();
+            System.out.println(v.toString());
+        }).start();
+        new Thread(()->{
+            AtomicInteger v=as.computeIfAbsent("a",k-> new AtomicInteger(0));
+            v.incrementAndGet();
+            System.out.println(v.toString());
+        }).start();
+        new Thread(()->{
+            AtomicInteger v=as.computeIfAbsent("a",k-> new AtomicInteger(0));
+            v.incrementAndGet();
+            System.out.println(v.toString());
+        }).start();
+
+        Thread.sleep(2000);
+        as.forEach((k,v)->{
+            System.out.println("key : "+k+", Value: "+v);
+        });
     }
 }
