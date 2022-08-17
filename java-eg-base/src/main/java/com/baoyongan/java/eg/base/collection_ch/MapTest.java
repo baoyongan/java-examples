0b76a7634a116ce2d6e8e774bcd7092b816cacbc
baoyongan <baoyongan@zhongchebaolian.com>
2022-08-17 10:39:42 +0800

    集合

--- java-eg-base/src/main/java/com/baoyongan/java/eg/base/collection_ch/MapTest.java
+++ java-eg-base/src/main/java/com/baoyongan/java/eg/base/collection_ch/MapTest.java
@@ -1,42 +1,50 @@
 package com.baoyongan.java.eg.base.collection_ch;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MapTest {
 
     public static void main(String[] args) {
 
-        Map<String,String> a=new HashMap();
+        Map<String, String> a = new HashMap();
         for (int i = 0; i < 1; i++) {
-            a.put("shashahaha"+i,i+"");
+            a.put("shashahaha" + i, i + "");
         }
 
-        for (Map.Entry<String,String> entry: a.entrySet()) {
-            System.out.println(entry.getKey()+"----"+entry.getValue());
+        for (Map.Entry<String, String> entry : a.entrySet()) {
+            System.out.println(entry.getKey() + "----" + entry.getValue());
         }
 
+
+        Hashtable s = new Hashtable();
+
+        ConcurrentHashMap ssd = new ConcurrentHashMap();
         System.out.println("注意null 的情况===================================");
 
-        Map<String,Object> b=new HashMap<>();
-        b.put("22",null);
-        b.put("11","11");
+        Map<String, Object> b = new HashMap<>();
+        b.put("22", null);
+        b.put("11", "11");
 
         System.out.println(b.get("22"));
         System.out.println(b.get("33"));
 
 
-        HashSet<String> ss=new HashSet<String>();
+        HashSet<String> ss = new HashSet<String>();
 
 
-        String[] al=new String[5];
-        al[0]="2";
+        String[] al = new String[5];
+        al[0] = "2";
         System.out.println(al);
-        int index=0;
-        do{}while(index<al.length && al[index++]==null);
+        int index = 0;
+        do {
+        } while (index < al.length && al[index++] == null);
         System.out.println(index);
 
 
-        Hashtable hashtable=new Hashtable();
+        Hashtable hashtable = new Hashtable();
+
+        Map<String, Integer> a1 = Collections.singletonMap("a", 65);
 
     }
 }
---
+++ java-eg-thread/src/main/java/com/baoyongan/java/eg/thread/concurrent/collection/LinekedListTest.java
@@ -0,0 +1,12 @@
+package com.baoyongan.java.eg.thread.concurrent.collection;
+
+import java.util.LinkedList;
+
+public class LinekedListTest {
+
+    public static void main(String[] args) {
+
+        LinkedList e=new LinkedList();
+//        e.add()
+    }
+}
